579a121dc3ca5064f82d2dfa98228756d9979ad2
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-14 15:07:17 -0300

    Resolve primeiro exercicio

---
+++ fundamentos/secao-02-introducao-a-html-e-css/dia-01-html-e-css-estruturas-de-página/sd-031-live-lecture/README.md
@@ -0,0 +1,18 @@
+# Repositório de aulas ao vivo para estudantes da Turma 31 😎
+
+Este repositório armazena os códigos e scripts fornecidos durante as aulas ao vivo pelas pessoas especialistas da Trybe.
+
+## Começando
+
+Basta clonar o repositório.
+
+```sh
+git clone git@github.com:tryber/sd-031-live-lecture.git
+```
+---
+
+### Estrutura
+
+Todos os conteúdos dados em aulas estarão no seu respectivo Pull Request! Aproveite esses conteúdos para estudar 😉
+
+---
---
+++ fundamentos/secao-02-introducao-a-html-e-css/dia-02-html-e-css-primeiros-passos-em-css/node_4b56ac883a405.tmp.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <title>HTML</title>
+    <style>h1{
+        font-size: 65px;
+        color: orange;
+        }
+        ul{
+            background-color: brown;
+        }
+        .cordefundo{
+            background-color: blueviolet;
+        }
+    </style>
+  </head>
+  <body>
+    <h1>Frutas</h1>
+    <p class="cordefundo">Qual é a sua fruta favorita?</p>
+    <ul class="cordefundo">
+      <li>Maçã</li>
+      <li>Banana</li>
+      <li>Goiaba</li>
+    </ul>
+  </body>
+</html>
