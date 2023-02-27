64eb5c34fa45e48d838b9a450121ad8f70723206
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-27 10:31:25 -0300

    Exercicio operadores logicos, and

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/operadores_logicos/and(&&)/script.js
@@ -0,0 +1,24 @@
+let currentHour = 17;
+let message = '';
+
+if (currentHour >= 22) {
+    message = 'Não comer nada, hora de dormir';
+}
+
+else if (currentHour >=18 && currentHour <=22) {
+    message = 'Hora do jantar';
+}
+
+else if (currentHour >= 14 && currentHour <18) {
+    message = 'Toma um café';
+}
+
+else if (currentHour >= 11 && currentHour < 14) {    
+    message = 'Hora do almoço';
+}
+
+else if (currentHour >= 4 && currentHour < 11) {
+    message = 'Cheirinho de café'
+}
+
+console.log (message)
