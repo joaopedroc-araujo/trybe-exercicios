c5a293769b0405116c1ba9ca19363abb318f4f33
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-27 15:28:07 -0300

    Adiciona exercicio switch/case

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/switch_case/script.js
@@ -0,0 +1,15 @@
+let grade = 'xablau';
+
+switch (grade) {
+    case 'aprovada':
+        console.log('Parabéns! Você foi aprovado');
+        break
+    case 'lista':
+        console.log('Você está na nossa lista de espera');
+        break
+    case 'reprovada':
+        console.log('Você foi reprovado!');
+        break
+    default:
+        console.log('Informação incorreta')
+}
