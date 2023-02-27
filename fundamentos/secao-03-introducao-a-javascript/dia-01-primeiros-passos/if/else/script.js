85ca1a7ea199e86c2860e556bd3131ad169bfec9
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-27 10:16:11 -0300

    Adiciona exercicio if/else

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/if/else/script.js
@@ -0,0 +1,11 @@
+let grade = 66;
+
+if (grade >= 80) {
+    console.log ('Parabéns, você está aprovado!')
+}
+else if (grade < 80 && grade >= 60) {
+    console.log ('Você está na nossa lista de espera!')
+}
+else {
+    console.log ('Você reprovou!')
+}
