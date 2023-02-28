87672d15efa3682d1a5f73716865c7f0e4a9da7c
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-28 09:54:06 -0300

    Adiciona exercicios

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos/pratica/script.js
@@ -0,0 +1,53 @@
+/*
+const number1 = 35;
+const number2 = 70;
+console.log('Os números são ' + number1 + 'e ' + number2)
+
+if (number1 > number2){
+    console.log ('Primeiro número maior que o segundo');
+}
+else {
+    console.log ('Segundo número maior que o primeiro');
+}
+
+
+const number1 = 20;
+const number2 = 35;
+const number3 = 89;
+
+console.log('Os números são ' + number1 + ', ' + number2 + ' e ' + number3);
+
+if (number1 > number2 || number1 > number3) {
+    console.log(number1);
+} else if (number2 > 1 || number2 > number3) {
+    console.log (number2)
+} else {
+    console.log(number3);
+}
+
+const number = 0;
+
+if (number > 0) {
+    console.log ('positive');
+} else if (number < 0) {
+    console.log('negative');
+} else if (number === 0) {
+    console.log('zero');
+}
+
+
+const angle1 = 80;
+const angle2 = 60;
+const angle3 = 40;
+let angles;
+
+if (angle1 + angle2 + angle3 === 180) {
+    angles = true;
+} else {
+    angles = false;
+}
+if (angles === true) {
+    console.log('Angulos validos');
+} else if (angles === false) {
+    console.log('Angulos invalidos');
+}*/
