cec7a18704bc36faf0522992b75e671910dc83db
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-02-28 10:25:28 -0300

    Adiciona exercicio for/of

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-loop-for/for_of/script.js
@@ -0,0 +1,12 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let eachName of names){
+    console.log(eachName);
+}
+
+let numbers = [1,2,3,4,5,6,7,8,9];
+
+for (let numbers1 of numbers) {
+    numbers1 += 1 
+    console.log(numbers1)
+}
