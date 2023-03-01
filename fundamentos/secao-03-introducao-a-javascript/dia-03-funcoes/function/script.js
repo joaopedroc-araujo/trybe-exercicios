a861a414c9885856e9f8f97eadb3179ae7e1a465
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-03-01 15:23:42 -0300

    Exercicios função e parametros

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-funcoes/function/script.js
@@ -0,0 +1,6 @@
+function verifyBalance (balance) {
+    return 'Olá cliente, seu saldo é de: ' + balance;
+}
+
+
+console.log (verifyBalance(7000 / 5));
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-funcoes/parametros_de_funcao/script.js
@@ -0,0 +1,18 @@
+//Faça um programa para adicionar clientes ao array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro caso o parâmetro não seja do tipo string.
+
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+// escreva aqui sua função
+function addClient(client){
+    if (typeof client === "string") {
+        clientesTrybeBank.push(client);
+        return 'Cliente adicionado com sucesso';
+    } else {
+        return 'Erro! Digite um nome válido!';
+    } 
+}
+
+console.log(addClient('Roberto'));
+console.log(addClient(13));
+console.log(addClient(true));
+console.log(addClient('Firmino'));
