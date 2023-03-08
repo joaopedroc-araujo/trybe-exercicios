8cac7edd1cc63681c03769fb0aaa47bd91b82237
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-03-08 20:26:44 -0300

    Adiciona exercios de seletores

---
+++ fundamentos/secao-04-dom-eventos-e-web-storage/dia-01-DOM-e-seletores/exercicios_capitulo/seletores/script.js
@@ -0,0 +1,22 @@
+const headerContainer = document.getElementById("header-container");
+const emergencyTasks = document.getElementsByClassName("emergency-tasks");
+const noEmergencyTasks = document.getElementsByClassName('no-emergency-tasks');
+const footerContainer = document.getElementById('footer-container');
+const changingH3 = document.querySelectorAll('h3');
+
+console.log(changingH3);
+
+const changeColors = () => {
+    headerContainer.style.backgroundColor = 'green';
+    emergencyTasks[0].style.backgroundColor = '#ff9f84';
+    noEmergencyTasks[0].style.backgroundColor = '#f9db5e';
+    changingH3[0].style.backgroundColor = 'purple';
+    changingH3[1].style.backgroundColor = 'purple';
+    changingH3[2].style.backgroundColor = 'black';
+    changingH3[3].style.backgroundColor = 'black';
+    footerContainer.style.backgroundColor = '#003533';
+    footerContainer.style.marginBottom = 0;
+}
+
+
+changeColors()
